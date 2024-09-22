74b5739a956f557d724c16d600903c9a283db001
MohamedWael21 <mohame171819@gmail.com>
2024-09-23 00:55:08 +0300

    add some package for protection

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,34 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.route";
 import cookieParser from "cookie-parser";
 import createHttpError from "http-errors";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 
 import globalErrorHandler from "./utils/global_error_handler";
 import { catchAsyncError } from "./utils/helpers";
 import workoutRoutes from "./routes/workout.route";
 import scheduleRoutes from "./routes/schedule.route";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(helmet());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(mongoSanitize());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+
+app.use(
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000, // 1h
+    message: "Too many requests from this IP, please try again in an hour!",
+  }),
+);
 
 app.use("/auth", authRoutes);
 app.use("/api/v1", workoutRoutes);
